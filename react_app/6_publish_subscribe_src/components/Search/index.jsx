b5d1b5a64ef7d3ff8c0a3d95c34fd2bc9686749c
Franco <ougaoyuan0718@gmail.com>
2022-12-12 23:16:19 +1100

    send request using fetch

--- react_app/6_publish_subscribe_src/components/Search/index.jsx
+++ react_app/6_publish_subscribe_src/components/Search/index.jsx
@@ -11,40 +11,28 @@ class Search extends Component {
         this.setState({keyword: e.target.value})
     }
 
-    search = async () => {
+    search = () =>{
 
 
-        if (this.state.keyword.trim() === '') {
+        if(this.state.keyword.trim() === ''){
             alert("Empty Input!")
             return
         }
         //update state before sending request
         // this.props.updateAppState({isFirst:false,isLoading:true})
-        PubSub.publish('franco', {isFirst: false, isLoading: true})
+        PubSub.publish('franco', {isFirst:false,isLoading:true})
 
-        //     axios.get('/api1/search/users?q=' + this.state.keyword).then(
-        //         r => {
-        //             // console.log(r.data)
-        //             // update the users using the data from the server
-        //             // this.props.updateAppState({isLoading:false,users:r.data.items})
-        //             PubSub.publish('franco', {isLoading:false,users:r.data.items})
-        //
-        //         },
-        //         e => PubSub.publish('franco', {isLoading:false,err:e.message})
-        //
-        // )
-        try {
-            const response = await fetch('/api1/search/users?q=' + this.state.keyword)
-            const result = await response.json()
-            console.log(result)
-            PubSub.publish('franco', {isLoading:false,users:result.items})
+        axios.get('/api1/search/users?q=' + this.state.keyword).then(
+            r => {
+                // console.log(r.data)
+                // update the users using the data from the server
+                // this.props.updateAppState({isLoading:false,users:r.data.items})
+                PubSub.publish('franco', {isLoading:false,users:r.data.items})
 
-        }catch (e){
-            console.log(e)
-            PubSub.publish('franco', {isLoading:false,err:e.message})
+            },
+            e => PubSub.publish('franco', {isLoading:false,err:e.message})
 
-        }
-
+    )
     }
 
     enterKey = (e) =>{
