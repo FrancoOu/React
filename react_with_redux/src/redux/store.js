78b7c0808c6f415df0eee041340e3b59cc8ec588
Franco <ougaoyuan0718@gmail.com>
2023-01-01 01:03:06 +1100

    create redux store using api from redux toolkit

--- react_with_redux/src/redux/store.js
+++ react_with_redux/src/redux/store.js
@@ -1,14 +1,16 @@
-// import createStore
-import {createStore, applyMiddleware, combineReducers} from "redux";
 // import count reducer
 import countReducer from './reducers/count'
 // import person reducer
 import personReducer from "./reducers/person";
 // import redux-thunk
 import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
+// import redux dev tools
+import {configureStore} from "@reduxjs/toolkit";
 
-const reducer = combineReducers({count:countReducer, people: personReducer})
-const store = createStore(reducer,composeWithDevTools(applyMiddleware(thunk)))
+// const reducer = combineReducers({count:countReducer, people: personReducer})
+// const store = createStore(reducer,composeWithDevTools(applyMiddleware(thunk)))
 
-export default store
+export default configureStore({
+    reducer:{count:countReducer, people: personReducer},
+    middleware:[thunk]
+})
